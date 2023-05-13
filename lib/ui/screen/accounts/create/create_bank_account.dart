e278a1f2fefa73a19ec4100aedda289d8bb1afb6
Subha <infolinematrix@gmail.com>
2023-05-13 19:45:41 +0530

    Subha

--- lib/ui/screen/accounts/create/create_bank_account.dart
+++ lib/ui/screen/accounts/create/create_bank_account.dart
@@ -1,16 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_wallet/models/accounts_model.dart';
 import 'package:flutter_wallet/ui/widgets/annotated_region.dart';
+
+import '../../../../util/ui_helpers.dart';
 
 class CreateBankAccount extends StatelessWidget {
   const CreateBankAccount({Key? key, required this.account}) : super(key: key);
   final AccountsModel account;
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormBuilderState>();
     return AnnotedAppRegion(
       child: Scaffold(
         appBar: AppBar(
           title: const Text("CREATE BANK ACCOUNT"),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+            child: FormBuilder(
+              key: formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          "ACCOUNT",
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium!
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Add your Bank accounts with available balance, it will  help you to calculate & show the current balance.",
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                      ],
+                    ),
+                  ),
+                  UIHelper.verticalSpaceMedium(),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/util/ui_helpers.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class UIHelper {
+  static const double _verticalExtraSmall = 4.0;
+  static const double _verticaleSmall = 8.0;
+  static const double _verticalMedium = 16.0;
+  static const double _verticalLarge = 24.0;
+  static const double _verticalExtraLarge = 32;
+  //--
+  static const double _horizontalExtraSmall = 4.0;
+  static const double _horizontalSmall = 8.0;
+  static const double _horizontalMedium = 16.0;
+  static const double _horizontalLarge = 24.0;
+  static const double _horizontalExtraLarge = 32.0;
+
+  //--
+  static SizedBox verticalSpace(double height) => SizedBox(height: height);
+  static SizedBox horizontalSpace(double width) => SizedBox(width: width);
+
+  //--Vertical
+  static SizedBox verticalSpaceExtraSmall() =>
+      verticalSpace(_verticalExtraSmall);
+  static SizedBox verticalSpaceSmall() => verticalSpace(_verticaleSmall);
+  static SizedBox verticalSpaceMedium() => verticalSpace(_verticalMedium);
+  static SizedBox verticalSpaceLarge() => verticalSpace(_verticalLarge);
+  static SizedBox verticalSpaceExtraLarge() =>
+      verticalSpace(_verticalExtraLarge);
+
+  //--Horizontal
+  static SizedBox horizontalSpaceExtraSmall() =>
+      horizontalSpace(_horizontalExtraSmall);
+  static SizedBox horizontalSpaceSmall() => horizontalSpace(_horizontalSmall);
+  static SizedBox horizontalSpaceMedium() => horizontalSpace(_horizontalMedium);
+  static SizedBox horizontalSpaceLarge() => horizontalSpace(_horizontalLarge);
+  static SizedBox horizontalSpaceExtraLarge() =>
+      horizontalSpace(_horizontalExtraLarge);
+}

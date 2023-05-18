90a1beb1cf3614bab8cf78d0ce52f8a7872d0e3f
Subha <infolinematrix@gmail.com>
2023-05-18 17:50:23 +0530

    Subha

--- lib/ui/screen/statement/statement_screen.dart
+++ lib/ui/screen/statement/statement_screen.dart
@@ -143,114 +143,12 @@ class StatementScreen extends StatelessWidget {
                           child: LinearProgressIndicator(),
                         ),
                       );
+                      
                     },
-                  )
+                  ),
                 ],
               ),
             ),
-            // body: SafeArea(
-            //   child: Padding(
-            //     padding: const EdgeInsets.only(left: 16, right: 16, top: 0),
-            //     child: CustomScrollView(
-            //       slivers: [
-            //         SliverToBoxAdapter(
-            //           child: Container(
-            //             padding: const EdgeInsets.symmetric(horizontal: 16),
-            //             decoration: BoxDecoration(
-            //               borderRadius: BorderRadius.circular(10),
-            //               color: Theme.of(context).cardColor,
-            //             ),
-            //             child: Row(
-            //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //               children: [
-            //                 Expanded(
-            //                   child: TextField(
-            //                     decoration: const InputDecoration(
-            //                       hintText: '12-Apr-2023 - 13-05-2023',
-            //                       border: InputBorder.none,
-            //                     ),
-            //                     style:
-            //                         Theme.of(context).textTheme.headlineMedium,
-            //                   ),
-            //                 ),
-            //                 Expanded(
-            //                   child: Align(
-            //                     alignment: Alignment.centerRight,
-            //                     child: InkWell(
-            //                       onTap: () => GoRouter.of(context).pop(),
-            //                       child: const Icon(Iconsax.calendar, size: 32),
-            //                     ),
-            //                   ),
-            //                 )
-            //               ],
-            //             ),
-            //           ),
-            //         ),
-            //         const SliverToBoxAdapter(
-            //           child: SizedBox(
-            //             height: 8,
-            //           ),
-            //         ),
-            //         SliverToBoxAdapter(
-            //           child: Column(
-            //             mainAxisSize: MainAxisSize.max,
-            //             crossAxisAlignment: CrossAxisAlignment.stretch,
-            //             children: [
-            //               Container(
-            //                 padding: const EdgeInsets.only(
-            //                     left: 16, right: 16, top: 8, bottom: 8),
-            //                 decoration: BoxDecoration(
-            //                   borderRadius: BorderRadius.circular(10),
-            //                   // color: Theme.of(context).cardColor,
-            //                 ),
-            //                 child: Row(
-            //                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //                   children: [
-            //                     Text("45,644.52 DR",
-            //                         style: Theme.of(context)
-            //                             .textTheme
-            //                             .headlineMedium),
-            //                     Text(
-            //                       "644.52 DR",
-            //                       style: Theme.of(context)
-            //                           .textTheme
-            //                           .headlineMedium!
-            //                           .copyWith(
-            //                             color:
-            //                                 Theme.of(context).colorScheme.error,
-            //                           ),
-            //                     ),
-            //                   ],
-            //                 ),
-            //               ),
-            //               const SizedBox(
-            //                 height: 16,
-            //               ),
-            //               // Container(
-            //               //   padding: const EdgeInsets.only(
-            //               //       left: 16, right: 16, top: 16, bottom: 16),
-            //               //   decoration: BoxDecoration(
-            //               //     borderRadius: BorderRadius.circular(10),
-            //               //     color: Theme.of(context).cardColor,
-            //               //   ),
-            //               // ),
-            //               ListView.builder(
-            //                 shrinkWrap: true,
-            //                 physics: const NeverScrollableScrollPhysics(),
-            //                 itemCount: 10,
-            //                 itemBuilder: (BuildContext context, int index) {
-            //                   return null;
-
-            //                   // return txnItem(context);
-            //                 },
-            //               ),
-            //             ],
-            //           ),
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // ),
           ),
         ),
       ),
